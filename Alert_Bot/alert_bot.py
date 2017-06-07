38ea694c4b2d282319d6b6503a6dc9d088780e70
pockygom <johnjaeryungsong@msn.com>
2017-06-07 12:19:10 -0400

    Fix.

--- Alert_Bot/alert_bot.py
+++ Alert_Bot/alert_bot.py
@@ -38,13 +38,11 @@ msg_interval = 60 # Seconds
 # Function for sendng message/attachments
 def send_msg(message, attachment, chan, now, last_sent):
 	# Wait until message interval passes (prevent spam)
-	print(time_since_last_msg.seconds)
 	if now == last_sent: # Wait until next second
 		wait_time = msg_interval - time_since_last_msg.seconds
 		print('Waiting for ' + str(wait_time) + ' seconds until sending message.')
 		th = Timer(wait_time, send_msg, [message, attachment, chan, now, last_sent])
 		th.start()
-		return(send_time)
 
 	# Send message to Slack
 	if not isinstance(message,str):
