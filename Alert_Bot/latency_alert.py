22f00c626c5fb464d50f6047e14ac33198612662
pockygom <johnjaeryungsong@msn.com>
2017-06-12 12:27:51 -0400

    Go

--- Alert_Bot/latency_alert.py
+++ Alert_Bot/latency_alert.py
@@ -93,7 +93,7 @@ def update_list(delta_thresh, past_delta_list=[], last_ref='-1'):
 	# Check if delta_thresh is an int
 	if not delta_thresh:
 		delta_thresh = 1800
-	elif:
+	else:
 		try:
 			int(delta_thresh)
 			is_int = True
